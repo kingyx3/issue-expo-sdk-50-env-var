9d8b94f23a95a9cb76d309786876a7d13d7ee2cd
jt <makeshiftplans@gmail.com>
2024-02-02 23:28:41 +0800

    Added text on test App.js

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>WELCOME</Text>
       <Text>{process.env.EXPO_PUBLIC_TEST}</Text>
+      <Text>BYE</Text>
       <StatusBar style="auto" />
     </View>
   );
